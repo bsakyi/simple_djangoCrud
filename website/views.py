ceb80f975408737c358bb1dfe030edc43a036403
swiches <donasare@live.com>
2023-03-08 20:19:39 +0000

    React Initial Setup Done

--- website/views.py
+++ website/views.py
@@ -7,6 +7,9 @@ from .models import Record
 
 
 # Create your views here.
+def index(request):
+    return render(request, 'frontend/index.html')
+
 def home(request):
     records = Record.objects.all()
     if request.method == 'POST':
@@ -23,7 +26,7 @@ def home(request):
             messages.success(request, "There was an error")
             return redirect('home') 
     else:
-        return render(request, 'home.html', {'records':records})
+        return render(request, 'frontend/home.html', {'records':records})
 
 # def login_user(request):
 #     pass
@@ -47,15 +50,15 @@ def register_user(request):
             return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form': form})
+        return render(request, 'frontend/register.html', {'form': form})
     
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'frontend/register.html', {'form': form})
 
 def customer_record(request, pk):
     if request.user.is_authenticated:
         #Lookup records
         customer_record = Record.objects.get(id=pk)
-        return render(request, 'record.html', {'customer_record': customer_record})
+        return render(request, 'frontend/record.html', {'customer_record': customer_record})
     else:
         messages.success(request, "You must be logged in to view this page")
         return redirect('home')
@@ -66,7 +69,7 @@ def delete_customer(request, pk):
         delete_it = Record.objects.get(id=pk)
         delete_it.delete()
         messages.success(request, "Record was deleted")
-        return render(request, 'home.html')
+        return render(request, 'frontend/home.html')
         
     else:
         messages.success(request, "You must be logged in to view this page")
@@ -81,7 +84,7 @@ def add_record(request):
                 add_record = form.save()
                 messages.success(request, "Record successfully added")
                 return redirect('home')
-        return render(request, 'add_record.html', {'form': form})
+        return render(request, 'frontend/add_record.html', {'form': form})
     else:
         messages.success(request, "You must be logged in")
         return redirect('home')
@@ -94,8 +97,8 @@ def update_record(request, pk):
         if form.is_valid():
             form.save()
             messages.success(request, "Record Has Been Updated")
-            return redirect('home')
-        return render(request, 'update_record.html', {'form': form})
+            return redirect('frontend/home')
+        return render(request, 'frontend/update_record.html', {'form': form})
     else:
         messages.success(request, "You must be logged in")
 
